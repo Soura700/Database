99964550152370cbd8a09f4f058bda5e0324d02c
Soura700 <108584756+Soura700@users.noreply.github.com>
2023-04-17 12:14:50 +0530

    Update auth.js

--- middlewire/auth.js
+++ middlewire/auth.js
@@ -1,33 +1,33 @@
-const isLogin = async (req, res, next) => {
-  try {
-    if (req.session.user_id) {
-      next();
-    } else {
-      return res.redirect("/log")
-    }
-  } catch (error) {
-  }
-}
-  
-  const isLogout = async (req,res,next)=>{
+const isLogin =async (req,res,next)=>{
     try {
-      if(req.session.user_id ){
-        next();
-      }
-      else{
+        // console.log("Success Islogin")
+      if(req.session.user_id){
+        
+      }else{
         return res.redirect("/log")
       }
       next();
     } catch (error) {
-
+      console.log(error)
+    }
+  }
+  
+  const isLogout = async (req,res,next)=>{
+    try {
+        // console.log("Success Islogout")
+      if(req.session.user_id ){
+       return res.redirect("/admin")
+      }
+      else{
+        // res.redirect("/login")
+      }
+      next();
+    } catch (error) {
+      console.log(error)
     }
   }
 
-
-
- 
   module.exports = {
     isLogin,
     isLogout
   }
-
